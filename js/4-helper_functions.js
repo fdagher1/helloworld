128d2c0f0781ddea2b4a0a4a683a346791db4945
Fadi Dagher <fdagher1@gmail.com>
2024-07-14 13:55:51 -0400

    Deleted function no longer being used

--- js/4-helper_functions.js
+++ js/4-helper_functions.js
@@ -20,43 +20,6 @@ function helperSortDictionaryIntoArray(dict) {
 
   return items;
 }
-  
-function helperCompareDates(date1, date2) {
-  if (date2 == "") {
-    return true;
-  }
-  
-  // If date2 is in format mm/yyyy then compare the month and year values in each variable
-  if (date2.length == 6 || date2.length == 7) { 
-    // Get the month and year from each date in order to compare them
-    var date1 = new Date(date1);
-    var date1_mm = (date1.getMonth()+1).toString();
-    var date1_yyyy = date1.getFullYear().toString();
-    var date2_mm = date2.split("/")[0];
-    var date2_yyyy = date2.split("/")[1];
-
-    // Check if mm/yyyy match and if so return true
-    if (date1_mm == date2_mm && date1_yyyy == date2_yyyy) {
-      return true;
-    }
-  // Otherwise check if the dates are an exact match
-  } else {
-    var date1 = new Date(date1);
-    //var date2 = new Date((parseInt(date2)+1).toString()) // Converting the date from yyyy to a full date, had to +1 as for some reason the year value of the date ends up a year older otherwise
-    //var date1_mm = (date1.getMonth()+1).toString();
-    var date1_yyyy = date1.getFullYear().toString();
-    //var date2_mm = (date2.getMonth()+1).toString();
-    //var date2_yyyy = date2.getFullYear().toString();
-
-    // Check if yyyy match and if so return true
-    if (date1_yyyy == date2) {
-      return true;
-    }
-  }
-
-  return false;
-
-}
 
 function helperGetTotalFigureAmountFromLine(line) {
   var total = 0;
