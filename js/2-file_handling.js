8a861b86d87cbd37189ee713600d88dc3ba8a0af
Fadi Dagher <fdagher1@gmail.com>
2024-08-25 21:57:53 -0400

    Code cleanup

--- js/2-file_handling.js
+++ js/2-file_handling.js
@@ -1,30 +1,20 @@
-
 async function readContentFromFile(event) {
-  // Get user entered password, if any
-  var enteredPassword = document.getElementById("textbox-password").value;
-
-  // Get file content
-  const file = event.target.files[0];
+  // GET FILE CONTENT, AND PASSWORD (IF ANY)
+  var enteredPassword = document.getElementById("textbox-password").value; // Get user entered password, if any
+  const file = event.target.files[0]; 
   if (enteredPassword == "") {
-    var reader = await file.text();
-  } else {
-    var reader = await file.arrayBuffer();
-  }
-
-  // If password was entered then decrypt the content of the read file
-  if (enteredPassword == "") {
+    var reader = await file.text(); // if not expected to be encrypted, then read file as text
     var decryptedCsvData = reader;
   } else {
-    var decryptedCsvData = await decrypt(reader);
+    var reader = await file.arrayBuffer(); // if expected to be encrypted, then read file as an array buffer
+    var decryptedCsvData = await decrypt(reader, enteredPassword); // decrypt file
   }
-  
-  // Convert content from string format to array
-  datasetArray = helperCsvToArray(decryptedCsvData);
+  datasetArray = helperCsvToArray(decryptedCsvData); // Convert content from string format to array
 
-  // Check if file format is valid 
+  // CHECK FILE VALIDITY
   var validationResult = validateFileFormatAndData(datasetArray);
 
-  // If so then proceed with displaying the top pane otherwise advise user to look in the browser's console for errors
+  // IF VALID THEN UPDATE APP, OTHERWISE DISPLAY ERROR
   if (validationResult == "No errors found.") {
     // Display Data in Top Pane
     retrieveDataForTopPane();
@@ -40,63 +30,41 @@ async function readContentFromFile(event) {
 }
 
 async function saveContentToFile() {
-  // GATHER USER INPUT    
+  // GATHER USER INPUT AND CREATE CORRESPONDING ARRAY
   var enteredPassword = document.getElementById("textbox-password").value; // Get user entered password, if any
   var enteredDate = document.getElementById("input-date").value; // Read date
   var enteredLocation = document.getElementById("input-location").value; // Read Location
   var enteredEvents = document.getElementById("input-events").value; // Read events
-
-  // CHECK IF OPERATION IS TO UPDATE EXISTING ENTRY OR TO ADD A NEW ONE
-  var result = helperReturnRowThatMatchesDate(datasetArray, enteredDate)
-
-  // CREATE ARRAY FROM INPUT
-  enteredDate = helperSetDateFormat(enteredDate); // Convert it to format Mon, 12/1/2024
+  enteredDate = helperSetDateFormat(enteredDate); // Convert date to format Mon, 12/1/2024
   enteredEvents = helperSetBeaklineCharacter(enteredEvents, "backslashnto<br>"); // Replace all \n with <br> as the code currently depends on that
   var userInput = [enteredDate, enteredLocation, enteredEvents];
 
   // VALIDATE USER INPUT ARRAY
+  var result = helperReturnRowThatMatchesDate(datasetArray, enteredDate) // Check if operation is to insert new line or to update existing line
   var validationResult = validateUserInputFormatAndData(datasetArray, userInput, result);
   if (validationResult != "No errors found.") {
     displayFileValidityError(validationResult);
     return;
   }
 
-  // SAVE FILE
-  // Check if date already exists and if so update the dataset with the new values, otherwise insert line
+  // CREATE FILE CONTENT
   if (result == "Date not found.") {
-    // Add user input to the dataset
-    datasetArray.unshift(userInput); 
+    datasetArray.unshift(userInput);  // If date alreadt exists, insert user data as a new line
   } else {
-    datasetArray = helperUpdateRowInDataset(datasetArray, userInput).slice();
+    datasetArray = helperUpdateRowInDataset(datasetArray, userInput).slice(); // Otherwise, update existing line
   }
+  var datasetCSV = helperArrayToCSV(datasetArray); // Convert dataset to CSV 
+  datasetCSV = "Day,Locations,Events,Thoughts\n" + datasetCSV; // Add headset to CSV
   
-  // Convert dataset to CSV and add headset
-  var datasetCSV = helperArrayToCSV(datasetArray); 
-  datasetCSV = "Day,Locations,Events,Thoughts\n" + datasetCSV;
-  
-  // Clear validity errors in case of any from previous save attemps attemps
-  clearFileValidityError();
-
-  // Switch back to read mode
-  eventAppModeButtonClicked();
-
-  // Update top pane
-  retrieveDataForTopPane();
-
-  // Redisplay the table
-  retrieveDataForUploadedFile();
-
-  // Encrypt data
-  var encryptedDatasetCSV = await encrypt(datasetCSV);
-  
+  // SAVE FILE
   // Set file content type depending on whether it is encrypted or not  
   if (enteredPassword == "") {
     var contentType = "text/plain";
+    var encryptedDatasetCSV = datasetCSV;
   } else {
     var contentType = "application/octet-stream";
+    var encryptedDatasetCSV = await encrypt(datasetCSV, enteredPassword);
   }
-  
-  // Save file
   var file = new Blob([encryptedDatasetCSV], { type: contentType });
   var link = document.createElement("a");
   var url = URL.createObjectURL(file);
@@ -108,48 +76,40 @@ async function saveContentToFile() {
     document.body.removeChild(link); 
     window.URL.revokeObjectURL(url);
   }, 0);
+
+  // UPDATE USER INTERFACE
+  clearFileValidityError(); // Clear validity errors in case of any from previous save attemps attemps
+  eventAppModeButtonClicked(); // Switch back to read mode
+  retrieveDataForTopPane(); // Update top pane
+  retrieveDataForUploadedFile();  // Redisplay the table 
 }
 
-async function decrypt(encryptedCsvData) {
-  // Get password
-  var enteredPassword = document.getElementById("textbox-password").value; // Read password
-  
-  if (enteredPassword == "") {
-    return encryptedCsvData;
-  } else {
-    try {
-      // Convert the password to a key using PBKDF2
-      const iv = new Uint8Array(12);
-      const keyMaterial = await crypto.subtle.importKey('raw', new TextEncoder().encode(enteredPassword), { name: 'PBKDF2' }, false, ['deriveBits', 'deriveKey']);
-      const key = await crypto.subtle.deriveKey({name: 'PBKDF2', salt: new Uint8Array(16), iterations: 100000, hash: 'SHA-256',}, keyMaterial, { name: 'AES-GCM', length: 256 }, true, ['encrypt', 'decrypt']);    
-  
-      // Decrypt the ciphertext
-      const decryptedData = await crypto.subtle.decrypt({ name: 'AES-GCM', iv }, key, encryptedCsvData);
-      return new TextDecoder().decode(decryptedData);
-    } catch (err) {
-      displayFileValidityError('Decryption failed:', err.message);
-    }
+async function decrypt(encryptedCsvData, enteredPassword) { 
+  try {
+    // Convert the password to a key using PBKDF2
+    const iv = new Uint8Array(12);
+    const keyMaterial = await crypto.subtle.importKey('raw', new TextEncoder().encode(enteredPassword), { name: 'PBKDF2' }, false, ['deriveBits', 'deriveKey']);
+    const key = await crypto.subtle.deriveKey({name: 'PBKDF2', salt: new Uint8Array(16), iterations: 100000, hash: 'SHA-256',}, keyMaterial, { name: 'AES-GCM', length: 256 }, true, ['encrypt', 'decrypt']);    
+
+    // Decrypt the ciphertext
+    const decryptedData = await crypto.subtle.decrypt({ name: 'AES-GCM', iv }, key, encryptedCsvData);
+    return new TextDecoder().decode(decryptedData);
+  } catch (err) {
+    displayFileValidityError('Decryption failed:', err.message);
   }
 }
 
-async function encrypt(datasetCSV) {
-  // Get password
-  var enteredPassword = document.getElementById("textbox-password").value; // Read password
-
-  if (enteredPassword == "") {
-    return datasetCSV;
-  } else {
-    try {
-      // Convert the password to a key using PBKDF2
-      const iv = new Uint8Array(12);
-      const keyMaterial = await crypto.subtle.importKey('raw', new TextEncoder().encode(enteredPassword), { name: 'PBKDF2' }, false, ['deriveBits', 'deriveKey']);
-      const key = await crypto.subtle.deriveKey({name: 'PBKDF2', salt: new Uint8Array(16), iterations: 100000, hash: 'SHA-256',}, keyMaterial, { name: 'AES-GCM', length: 256 }, true, ['encrypt', 'decrypt']);
-        
-      // Encrypt the plaintext
-      const encodedPlaintext = new TextEncoder().encode(datasetCSV);
-      return await crypto.subtle.encrypt({ name: 'AES-GCM', iv }, key, encodedPlaintext);
-    } catch (err) {
-      displayFileValidityError('Encryption failed:', err.message);
-    }
+async function encrypt(datasetCSV, enteredPassword) {
+  try {
+    // Convert the password to a key using PBKDF2
+    const iv = new Uint8Array(12);
+    const keyMaterial = await crypto.subtle.importKey('raw', new TextEncoder().encode(enteredPassword), { name: 'PBKDF2' }, false, ['deriveBits', 'deriveKey']);
+    const key = await crypto.subtle.deriveKey({name: 'PBKDF2', salt: new Uint8Array(16), iterations: 100000, hash: 'SHA-256',}, keyMaterial, { name: 'AES-GCM', length: 256 }, true, ['encrypt', 'decrypt']);
+      
+    // Encrypt the plaintext
+    const encodedPlaintext = new TextEncoder().encode(datasetCSV);
+    return await crypto.subtle.encrypt({ name: 'AES-GCM', iv }, key, encodedPlaintext);
+  } catch (err) {
+    displayFileValidityError('Encryption failed:', err.message);
   }
 }
